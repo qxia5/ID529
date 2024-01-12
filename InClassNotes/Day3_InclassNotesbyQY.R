c5b980b1c2472a62ef41f160414b23e1bab83d9e
Qianyi Xia <qianyixia@hotmail.com>
2024-01-11 22:58:45 -0500

    Day4

--- InClassNotes/Day3_InclassNotesbyQY.R
+++ InClassNotes/Day3_InclassNotesbyQY.R
@@ -45,14 +45,14 @@ stringr::str_extract(
 
 
 #Data cleaning with data messy_data
-install.packages("pacman")
+
 library(pacman)
 library(janitor)
 p_load(tidyverse)
 p_load(scales)
 
 # Read in the messy data from csv
-messy_data <- read_csv("messy_data.csv")
+messy_data <- read_csv("data/messy_data.csv")
 
 # Clean column names ------------------------------------------------------
 
@@ -97,6 +97,115 @@ dftest <- df |>
   select(-missing_count)
 
 dim(dftest)
+
+# Collapsing categories ---------------------------------------------------
+
+
+# We would like to do some collapsing of these variables:
+# 1. Create a combined race/ethnicity variable from race_self_identified and hispanic_ethnicity
+# This new variable should combine Asian and Native Hawaiian and Pacific Islander individuals into
+# the same category. 
+# 2. Create a collapsed gender variable with categories cisgender female, cisgender male, and trans/non-binary.
+# 3. Create a collapsed education variable with categories less than high school, high school graduate, and college graduate.
+
+table(unique_df$race_self_identified, unique_df$hispanic_ethnicity, useNA="ifany")
+
+
+unique_df <- unique_df |>
+  mutate(raceth = factor(   #change to factor then you can use white as reference factor later
+    case_when(
+      race_self_identified == "White" &
+        hispanic_ethnicity == "Non-Hispanic" ~ "Non-Hispanic White",
+      race_self_identified == "Black" &
+        hispanic_ethnicity == "Non-Hispanic" ~ "Non-Hispanic Black",
+      hispanic_ethnicity == "Hispanic" ~ "Hispanic",
+      is.na(race_self_identified) |
+        is.na(hispanic_ethnicity) ~ NA_character_,
+      TRUE ~ "underrepresented racial/ethnic group"
+    ),
+    levels = c(
+      "Non-Hispanic White",
+      "Non-Hispanic Black",
+      "Hispanic",
+      "underrepresented racial/ethnic group"
+    )
+  ))
+
+
+unique_df <- unique_df |>
+  mutate(
+    # recode gender collapsed
+    gender_collapsed = if_else(
+      self_identified_gender %in% c(
+        "transgender female",
+        "transgender male",
+        "non-binary or genderqueer"
+      ),
+      "trans/non-binary",
+      self_identified_gender
+    ),
+    education_collapsed = case_when(
+      highest_education_completed %in% c("8th Grade", "9-11th Grade") ~ "less than hs",
+      highest_education_completed %in% c("High School", "Some College") ~ "hs grad",
+      highest_education_completed == "College Grad" ~ "college grad"
+    )
+  )
+
+# Create categorical variables from continuous variables ------------------
+
+# We would like to create a dichotomous sleep variable where 0 = >=7 hours of sleep per night and 1= <7 hours of sleep per night
+# We would also like to create a categorical income variable with categories <$35,000, $35000-$50000, $50000-$100000, and >=$100000
+# We would also like to create an income quintile variable
+
+
+unique_df <- unique_df |>
+  mutate(insufficient_sleep = hours_of_sleep_per_night < 7)
+
+table(unique_df$insufficient_sleep)
+
+unique_df <- unique_df |>
+  mutate(income_cat = cut(household_income_before_taxes, 
+                          breaks = c(0, 25000, 50000, 75000, 100000, 1000000),
+                          right = FALSE, include.lowest=TRUE, na.rm=T),
+         income_quint = ntile(household_income_before_taxes, 5))
+
+
+# Let's visualize how income_cat has split up the household_income_before_taxes variable
+ggplot(unique_df, aes(x=income_cat, y=household_income_before_taxes)) + 
+  geom_boxplot() +
+  scale_y_continuous(labels=label_comma())
+
+# Let's compare how income_quint has split up the household_income_before_taxes variable
+ggplot(unique_df, aes(x=factor(income_quint), y=household_income_before_taxes)) + geom_boxplot()
+
+# Use group_by and summarize ----------------------------------------------
+
+
+# to see the minimum and maximum of the quintile categories created by ntile()
+# we can use group_by and summarize
+unique_df |>
+  group_by(income_quint) |>
+  summarise(mininc = min(household_income_before_taxes, na.rm=T),
+            maxinc = max(household_income_before_taxes, na.rm=T))
+
+
+# suppose that we want to calculate the proportion of subjects
+# with insufficient sleep in each gender and racial/ethnic group
+
+proportions_by_gender_raceth <- unique_df |>
+  # group by gender, race/ethnicity, and insufficient sleep category
+  group_by(gender_collapsed, raceth, insufficient_sleep) |>
+  # count the number of subjects in each category
+  # Note the use of .groups='drop' which removes the group_by after summarizing
+  summarise(count_insufficient_sleep = n(), .groups='drop') |>
+  # Now group by gender and race/ethnicity
+  group_by(gender_collapsed, raceth) |>
+  # Calculate the proportion
+  mutate(proportion_insufficient_sleep = count_insufficient_sleep/sum(count_insufficient_sleep, na.rm=T)) |>
+  # Remove the grouping
+  ungroup()
+
+head(proportions_by_gender_raceth)
 
 
 # About Function ----------------------------------------------------------
--- InClassNotes/Day4_InclassNotesbyQY.R
+++ InClassNotes/Day4_InclassNotesbyQY.R
@@ -2,3 +2,38 @@
 install.packages("rvest")
 library(rvest)
 literacy <-read_html()
+
+nhanes_id529$bp_cat <- case_when(nhanes_id529$mean_BP > 140 ~ "high BP",
+                                 nhanes_id529$mean_BP >= 90 &nhanes_id529$mean_BP <= 140 ~ "normal BP",
+                                 nhanes_id529$mean_BP <= 90 ~ "low BP"
+)
+
+table(nhanes_id529$bp_cat, useNA = "ifany")
+
+
+# Mapping -----------------------------------------------------------------
+
+install.packages('tigris')
+
+library(tigris)
+library(ggplot2)
+
+manhattan_roads <- roads("NY", "New York")
+
+ggplot(manhattan_roads) + 
+  geom_sf() + 
+  theme_void()
+library(tidyverse)
+library(sf)
+
+# you might have some data in a shapefile -- 
+# data/shapefile_unzipped/
+#  -- cb_2013_us_county_20m.dbf
+#  -- cb_2013_us_county_20m.prj
+#  -- cb_2013_us_county_20m.shp
+#  -- cb_2013_us_county_20m.shp.iso.xml
+#  -- cb_2013_us_county_20m.shp.xml
+#  -- cb_2013_us_county_20m.shx
+#  -- county_20m.ea.iso.xml
+
+counties <- read_sf(here("data/shapefile_unzipped"))
